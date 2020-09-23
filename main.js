00fae67d2460011b5768b5129f4f86f9ce3b602e
zifengb <987400803@qq.com>
2020-09-23 10:31:16 +0800

    改变为 range 操作 DOM, 完成重新渲染和 setState 合并

--- main.js
+++ main.js
@@ -1,43 +1,143 @@
 import { createElement, Component, render } from './toy-react.js'
 
-// let el = <div id="a" class="b" style={{color: 'red'}}>
-//   12312
-//   <div>1213</div>
-//   <div>31e41<span>kkk</span></div>
-//   <div>312</div>
-//   {[<div>fasdf</div>]}
-// </div>
-
-
-class MyComponent extends Component {
-  constructor() {
-    super()
-  }
-  render () {
-    return <div id='k' class={['h h1', 'j']} style={{ fontSize: '16px' }}>
-      <h1>My Component</h1>
-      {this.children}
-    </div>
+class Square extends Component {
+  render() {
+    return (
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}>
+        {this.props.value}
+      </button>
+    );
   }
 }
 
-const el = <MyComponent id='a'
-  class={['b', 'b1', ['c', 'c1 c2', {c3: true}]]}
-  style={['border: 1px solid #fff; backgroundColor: blue', { color: 'red' }]}>
-<div>
-  {[
-    <div>1</div>,
-    <div>2</div>,
-    [
-      <div>3</div>,
-      [
-        <div>3.1</div>
-      ]
-    ]
-  ]}
-</div>
-</MyComponent>
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
 
-console.log(el)
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (
+      squares[a]&&
+      squares[a] === squares[b] &&
+      squares[b] === squares[c]
+    ) {
+      return squares[a]
+    }
+  }
 
-render(el, document.body)
+  return null
+}
+
+class Board extends Component {
+  renderSquare(i) {
+    return <Square
+      value={this.props.squares[i]}
+      onClick={() => this.props.onClick(i)}
+    />;
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      xIsNext: true,
+      stepNumber: 0,
+      history: [{
+        squares: Array(9).fill(null)
+      }]
+    }
+  }
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
+    const squares = history[history.length - 1].squares.slice()
+    const flag = this.state.xIsNext
+    if (calculateWinner(squares) || squares[i]) { return }
+    squares[i] = flag ? 'X' : 'O'
+    this.setState({ history: history.concat({ squares }), stepNumber: history.length, xIsNext: !flag })
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    })
+  }
+
+  render() {
+    const history = this.state.history
+    const current = history[this.state.stepNumber]
+    const winner = calculateWinner(current.squares)
+
+    const moves = history.map((step, move) => {
+      const desc = move ? `Go to move #${move}` : `Go to game start`
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    })
+
+    let status
+    if (winner) {
+      status = `Winner: ${winner}`
+    } else {
+      status = `Next Player: ${this.state.xIsNext ? 'X' : 'O'}`
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={i => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+render(
+  <Game />,
+  document.getElementById('root')
+);
